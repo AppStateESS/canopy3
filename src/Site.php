4df0e2d184ef7763f1962cfebe566040993bf0d3
Matt McNaney <mcnaneym@appstate.edu>
2020-04-27 13:38:42 -0400

    Site stub

---
+++ src/Site.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * MIT License
+ * Copyright (c) 2020 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace Canopy3;
+
+class Site
+{
+
+    public static function view(int $siteId)
+    {
+        echo 'hello';
+    }
+
+}

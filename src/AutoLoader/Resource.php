ed4a3105fb6f7db447418de427c63c7419b82c88
Matt McNaney <mcnaneym@appstate.edu>
2020-04-30 14:00:17 -0400

    Sorted functions

--- src/AutoLoader/Resource.php
+++ src/AutoLoader/Resource.php
@@ -9,17 +9,17 @@
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
  */
+function DashboardLoader($fileName, $directory)
+{
+    require_once C3_ROOT . 'resources/dashboards/src/' . $directory . '/' . $fileName . '.php';
+}
+
 function PluginLoader($fileName, $directory)
 {
-    require_once C3_ROOT . 'resources/plugins/src' . $directory . $fileName . '.php';
+    require_once C3_ROOT . 'resources/plugins/src/' . $directory . '/' . $fileName . '.php';
 }
 
 function ThemeLoader($fileName, $directory)
 {
-    require_once C3_ROOT . 'resources/themes/src' . $directory . $fileName . '.php';
-}
-
-function DashboardLoader($fileName, $directory)
-{
-    require_once C3_ROOT . 'resources/dashboards/src' . $directory . $fileName . '.php';
+    require_once C3_ROOT . 'resources/themes/src/' . $directory . '/' . $fileName . '.php';
 }

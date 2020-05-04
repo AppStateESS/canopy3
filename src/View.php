8f1e9dffbdd99796da4651e7c1c6f597f4f94a15
Matt McNaney <mcnaneym@appstate.edu>
2020-05-04 13:43:53 -0400

    Beginning of View class

--- src/View.php
+++ src/View.php
@@ -14,5 +14,29 @@ namespace Canopy3;
 
 class View
 {
-//put your code here
+
+    static $theme = C3_DEFAULT_THEME;
+
+    public static function themed(array $sections, string $themeName)
+    {
+        $theme = new Theme;
+        $theme->sections = $sections;
+        return $theme->print();
+    }
+
+    public static function html(string $content)
+    {
+
+    }
+
+    public static function json(array $jsonArray)
+    {
+        return json_encode($jsonArray);
+    }
+
+    public static function error(string $message, int $code)
+    {
+
+    }
+
 }

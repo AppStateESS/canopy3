20d084eca6725b267f7e6c2a2ed9b4b951825ee7
Matt McNaney <mcnaneym@appstate.edu>
2020-05-04 13:44:06 -0400

    Stub for Theme class

---
+++ src/Theme.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * MIT License
+ * Copyright (c) 2020 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace Canopy3;
+
+class Theme
+{
+
+    private $footer;
+    private $header;
+    private $sections;
+    private $themeFolder;
+
+    public function __construct()
+    {
+
+    }
+
+    public function frontPage()
+    {
+
+    }
+
+    public function internalPage()
+    {
+
+    }
+
+    public function basicPage()
+    {
+
+    }
+
+    private function loadSections()
+    {
+
+    }
+
+}

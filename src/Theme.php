ec96fb56ea658678645cee6a8f46e725eaee3418
Matt McNaney <mcnaneym@appstate.edu>
2020-05-08 13:07:41 -0400

    Work in progress for theming

--- src/Theme.php
+++ src/Theme.php
@@ -12,17 +12,35 @@
 
 namespace Canopy3;
 
+require_once C3_ROOT . 'config/theme.php';
+
+if (!is_defined('C3_DEFAULT_THEME')) {
+    define('C3_DEFAULT_THEME', 'Simple');
+}
+
 class Theme
 {
 
     private $footer;
     private $header;
     private $sections;
-    private $themeFolder;
+    private $themeName;
+    private $structure;
 
-    public function __construct()
+    public function __construct(string $themeName = C3_DEFAULT_THEME)
     {
+        $this->themeName = trim($themeName);
+        $this->load();
+    }
 
+    private function load()
+    {
+        $filePath = C3_ROOT . 'themes/' . $this->themeName . '/structure.json';
+        if (!is_file($filePath)) {
+            throw new \Exception("Theme {$this->themeName} structure file not found");
+        }
+        $structure = file_get_contents($filePath);
+        $this->structure = json_decode($structure);
     }
 
     public function frontPage()

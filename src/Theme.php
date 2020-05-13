dd6b8d1e56acefb884abf794c1bf49f8ca1ab2c9
Matt McNaney <mcnaneym@appstate.edu>
2020-05-13 14:24:09 -0400

    Structure and Page added. Work in progress.

--- src/Theme.php
+++ src/Theme.php
@@ -12,55 +12,109 @@
 
 namespace Canopy3;
 
+use Canopy3\Exception\FileNotFound;
+use Canopy3\Theme\Structure;
+use Canopy3\HTTP\Header;
+use Canopy3\HTTP\Footer;
+
 require_once C3_ROOT . 'config/theme.php';
 
-if (!is_defined('C3_DEFAULT_THEME')) {
-    define('C3_DEFAULT_THEME', 'Simple');
+
+if (!defined('C3_DEFAULT_THEME')) {
+    define('C3_DEFAULT_THEME', 'canopy3-theme-simple');
 }
 
 class Theme
 {
 
-    private $footer;
-    private $header;
-    private $sections;
-    private $themeName;
-    private $structure;
+    static \Canopy3\Theme $singleton;
+    //private array $sections; ??
+    public string $themeName;
 
-    public function __construct(string $themeName = C3_DEFAULT_THEME)
+    /**
+     *
+     * @var Canopy3\Theme\Structure
+     */
+    private Structure $structure;
+
+//    private $frontPageTemplate;
+//    private $internalTemplate;
+//    private $errorTemplate;
+
+
+    private function __construct(string $themeName = C3_DEFAULT_THEME,
+            string $page = null)
     {
         $this->themeName = trim($themeName);
-        $this->load();
+        $this->loadStructure($page);
     }
 
-    private function load()
+    public static function singleton()
     {
-        $filePath = C3_ROOT . 'themes/' . $this->themeName . '/structure.json';
-        if (!is_file($filePath)) {
-            throw new \Exception("Theme {$this->themeName} structure file not found");
+        if (empty(self::$singleton)) {
+            self::$singleton = new self;
         }
-        $structure = file_get_contents($filePath);
-        $this->structure = json_decode($structure);
+        return self::$singleton;
     }
 
-    public function frontPage()
+    private function loadStructure(string $page = null)
     {
+        $this->structure = new Structure;
+        $filePath = C3_ROOT . 'resources/themes/' . $this->themeName . '/structure.json';
+        if (!is_file($filePath)) {
+            throw new FileNotFound($filePath);
+        }
+        $structureJson = file_get_contents($filePath);
+        $structureObj = json_decode($structureJson);
 
+        $this->structure->setTitle($structureObj->title);
+        $this->structure->directory = $structureObj->directory;
+        $this->structure->setDescription($structureObj->description);
+        $this->structure->setScreenshot($structureObj->screenshot);
+        $this->structure->setPages($structureObj->pages);
+        $this->structure->setDefaultPage($structureObj->defaultPage);
+        if ($page && $this->structure->pageExists($page)) {
+            $this->structure->setCurrentPage($page);
+        }
     }
 
-    public function internalPage()
+    public function addContent(string $content, string $section = null)
     {
-
+        if (empty($section)) {
+            $section = 'main';
+        }
+        $this->sectionContent[$section][] = $content;
     }
 
-    public function basicPage()
+    public function getContent()
     {
-
+        foreach ($this->sectionContent as $sectionName => $sectionContent) {
+            $sections[$sectionName] = implode("\n", $sectionContent);
+        }
+        return $sections;
     }
 
-    private function loadSections()
+    public function view()
     {
+        $template = new Template($this->getThemePagesDirectory());
+        $fileName = $this->structure->currentPage->filename;
+        $header = Header::singleton();
+        $footer = Footer::singleton();
+        $values = $this->getContent();
+        $values['header'] = $header->view();
+        $values['footer'] = $footer->view();
 
+        return $template->render($fileName, $values);
+    }
+
+    public function getThemeDirectory()
+    {
+        return 'resources/themes/' . $this->structure->directory;
+    }
+
+    public function getThemePagesDirectory()
+    {
+        return $this->getThemeDirectory() . '/pages/';
     }
 
 }

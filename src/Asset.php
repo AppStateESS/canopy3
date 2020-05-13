4f20bcbc8e4edb79c23fe368d9b25fa7930b33d9
Matt McNaney <mcnaneym@appstate.edu>
2020-05-13 14:20:07 -0400

    Asset stub

---
+++ src/Asset.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * MIT License
+ * Copyright (c) 2020 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace Canopy3;
+
+class Asset
+{
+
+    protected int $id;
+    protected string $site;
+    protected string $title;
+    protected string $thumbnail;
+    protected string $summary;
+    protected string $urlTitle;
+    protected int $createdOn;
+    protected string $callView;
+
+}

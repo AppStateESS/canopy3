7620075c5c2e0feb0f90c69711adad872fd16fae
Matt McNaney <mcnaneym@appstate.edu>
2020-05-13 14:15:19 -0400

    Renamed Simple theme to canopy3-theme-simple

--- config/theme.php
+++ config/theme.php
@@ -9,4 +9,4 @@
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
  */
-define('C3_DEFAULT_THEME', 'Simple');
+define('C3_DEFAULT_THEME', 'canopy3-theme-simple');

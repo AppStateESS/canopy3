034db19e0085ca21952f351f7dcf20f0e7a57f97
Matt McNaney <mcnaneym@appstate.edu>
2020-05-19 14:01:43 -0400

    Shifted variable names.

--- src/Asset.php
+++ src/Asset.php
@@ -1,10 +1,7 @@
 <?php
 
 /**
- * MIT License
- * Copyright (c) 2020 Electronic Student Services @ Appalachian State University
  *
- * See LICENSE file in root directory for copyright and distribution permissions.
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
@@ -16,12 +13,28 @@ class Asset
 {
 
     protected int $id;
-    protected string $site;
-    protected string $title;
-    protected string $thumbnail;
-    protected string $summary;
-    protected string $urlTitle;
-    protected int $createdOn;
     protected string $callView;
+    protected int $createdOn;
+
+    /**
+     * Title sent to the <title> tag in the header
+     * @var string
+     */
+    protected string $browserTitle;
+    protected string $site;
+    protected string $summary;
+    protected string $thumbnail;
+
+    /**
+     * Full title of asset
+     * @var string
+     */
+    protected string $title;
+
+    /**
+     * URL dashed title used to access asset.
+     * @var string
+     */
+    protected string $urlTitle;
 
 }

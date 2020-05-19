4950e5ea5e31221e0620c89a599dd7677f78a7ff
Matt McNaney <mcnaneym@appstate.edu>
2020-05-19 13:59:38 -0400

    New functionality added.

--- src/HTTP/Header.php
+++ src/HTTP/Header.php
@@ -1,13 +1,7 @@
 <?php
 
 /**
- *
  * The Head class collects meta information for the <head> tag of an html page.
- *
- * MIT License
- * Copyright (c) 2020 Electronic Student Services @ Appalachian State University
- *
- * See LICENSE file in root directory for copyright and distribution permissions.
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
@@ -15,10 +9,20 @@
 
 namespace Canopy3\HTTP;
 
+use Canopy3\Variable\StringVar;
+
 class Header extends AbstractMetaData
 {
 
     private static $header;
+    private StringVar $siteTitle;
+    private StringVar $pageTitle;
+
+    public function __construct()
+    {
+        $this->siteTitle = new StringVar;
+        $this->pageTitle = new StringVar;
+    }
 
     public static function singleton(): object
     {
@@ -28,11 +32,32 @@ class Header extends AbstractMetaData
         return self::$header;
     }
 
+    public function setSiteTitle($title)
+    {
+        $this->sitetitle->set($title);
+    }
+
+    public function setPageTitle($title)
+    {
+        $this->pageTitle->set($title);
+    }
+
+    public function getFullTitle()
+    {
+        $tag = new \Canopy3\Tag(null, 'title');
+        if ($this->pageTitle->isEmpty()) {
+            $tag->child = $this->pageTitle->get();
+        } else {
+            $tag->child = $this->pagetitle->get . ' - ' . $this->siteTitle->get();
+        }
+        return $tag->print();
+    }
+
     public function view()
     {
-        $robots = (string) Robots::singleton();
-
-        return 'header';
+        $values[] = (string) Robots::singleton();
+        $values[] = $this->getFullTitle();
+        return implode("\n", $values);
     }
 
 }

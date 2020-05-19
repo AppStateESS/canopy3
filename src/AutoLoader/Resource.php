05ee04cef6cf7b6b97827b11c8026d7b35826fef
Matt McNaney <mcnaneym@appstate.edu>
2020-05-19 14:02:42 -0400

    C3_ROOT changed to C3_DIR

--- src/AutoLoader/Resource.php
+++ src/AutoLoader/Resource.php
@@ -1,20 +1,17 @@
 <?php
 
 /**
- * MIT License
- * Copyright (c) 2020 Electronic Student Services @ Appalachian State University
  *
- * See LICENSE file in root directory for copyright and distribution permissions.
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
  */
 function DashboardLoader(string $fileName, string $directory)
 {
-    require_once C3_ROOT . 'resources/dashboards/src/' . $directory . '/' . $fileName . '.php';
+    require_once C3_DIR . 'resources/dashboards/src/' . $directory . '/' . $fileName . '.php';
 }
 
 function PluginLoader(string $fileName, string $directory)
 {
-    require_once C3_ROOT . 'resources/plugins/src/' . $directory . '/' . $fileName . '.php';
+    require_once C3_DIR . 'resources/plugins/src/' . $directory . '/' . $fileName . '.php';
 }

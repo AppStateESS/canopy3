3ecdf7b4c47d9070d2ffc77fbb084ae3a9583d89
Matt McNaney <mcnaneym@appstate.edu>
2020-05-20 13:57:10 -0400

    Rearranged autoloader directory
    The format is now
    Dashboard\NameOfDashboard\ClassName
    Plugin\NameOfPlugin\ClassName

--- src/AutoLoader/Resource.php
+++ src/AutoLoader/Resource.php
@@ -8,10 +8,10 @@
  */
 function DashboardLoader(string $fileName, string $directory)
 {
-    require_once C3_DIR . 'resources/dashboards/src/' . $directory . '/' . $fileName . '.php';
+    require_once C3_DIR . 'resources/dashboards/' . $directory . '/src/' . $fileName . '.php';
 }
 
 function PluginLoader(string $fileName, string $directory)
 {
-    require_once C3_DIR . 'resources/plugins/src/' . $directory . '/' . $fileName . '.php';
+    require_once C3_DIR . 'resources/plugins/' . $directory . '/src/' . $fileName . '.php';
 }

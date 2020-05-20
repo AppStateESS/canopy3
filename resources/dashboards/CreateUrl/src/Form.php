de77fea94b1a18c9934ba1166ba45ed0efab0232
Matt McNaney <mcnaneym@appstate.edu>
2020-05-20 14:05:36 -0400

    Stub

---
+++ resources/dashboards/CreateUrl/src/Form.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace Dashboard\CreateUrl;
+
+class Form
+{
+
+    public static function view()
+    {
+        echo 'view working';
+    }
+
+}

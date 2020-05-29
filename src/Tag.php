efb6c0a1e1c3871046261b867050df850d121195
Matt McNaney <mcnaneym@appstate.edu>
2020-05-29 14:44:55 -0400

    singleton variable added. Determines start and end tag usage.
    + paramName changed to attributeName
    + params changed to attributes
    + Added boolAttributes for attributes that appear without assignment (=)

--- src/Tag.php
+++ src/Tag.php
@@ -13,22 +13,29 @@ class Tag
 {
 
     public ?string $child = null;
-    protected array $params = [];
+    protected array $attributes = [];
     protected string $tagName;
+    protected array $boolAttributes = [];
 
-    public function __call(string $paramName, array $paramValues)
+    /**
+     * If $singleton == false, start and end tags are used.
+     * @var type
+     */
+    protected bool $singleton = false;
+
+    public function __call(string $attributeName, array $attributeValues)
     {
-        if (isset($this->params[$paramName])) {
-            $this->params[$paramName] = implode('', $paramValues);
+        if (isset($this->attributes[$attributeName])) {
+            $this->attributes[$attributeName] = implode('', $attributeValues);
         }
         return $this;
     }
 
-    public function __construct(array $params = null, string $tagName = null,
-            string $child = null)
+    public function __construct(array $attributes = null,
+            string $tagName = null, string $child = null)
     {
         $this->tagName = $tagName ?? $this->tagName ?? $this->classToTagName();
-        $this->params = $params ?? [];
+        $this->attributes = $attributes ?? [];
         if (!empty($child)) {
             $this->child = $child;
         }
@@ -39,9 +46,14 @@ class Tag
         return $this->print();
     }
 
-    public function setParam(string $paramName, string $value)
+    public function setBoolAttribute(string $attributeName, $status = true)
     {
-        $this->params[$paramName] = $value;
+        $this->boolAttributes[$attributeName] = $status;
+    }
+
+    public function setAttribute(string $attributeName, string $value)
+    {
+        $this->attributes[$attributeName] = $value;
     }
 
     public static function build(string $tagName)
@@ -56,14 +68,14 @@ class Tag
 
     public function print()
     {
-        if (is_null($this->child)) {
+        if (is_null($this->child) && $this->singleton) {
             return <<<EOF
-<{$this->tagName}{$this->parameters()}/>
+<{$this->tagName}{$this->listAttributes()}/>
 EOF;
         } else {
             return
                     <<<EOF
-<{$this->tagName}{$this->parameters()}>{$this->child}</{$this->tagName}>
+<{$this->tagName}{$this->listAttributes()}>{$this->child}</{$this->tagName}>
 EOF;
         }
     }
@@ -74,17 +86,28 @@ EOF;
         return strtolower(array_pop($namespaceArray));
     }
 
-    private function parameters()
+    private function listAttributes()
     {
-        if (empty($this->params)) {
-            return;
+        if (empty($this->attributes) && empty($this->boolAttributes)) {
+            return null;
         }
-        foreach ($this->params as $param => $value) {
-            if (!is_null($value)) {
-                $paramList[] = "$param=\"$value\"";
+
+        if (!empty($this->attributes)) {
+            foreach ($this->attributes as $attribute => $value) {
+                if (!is_null($value)) {
+                    $attributeList[] = "$attribute=\"$value\"";
+                }
             }
         }
-        return ' ' . implode(' ', $paramList) . ' ';
+
+        if (!empty($this->boolAttributes)) {
+            foreach ($this->boolAttributes as $attributeName => $status) {
+                if ($status) {
+                    $attributeList[] = $attributeName;
+                }
+            }
+        }
+        return ' ' . implode(' ', $attributeList);
     }
 
 }

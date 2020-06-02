00d69d40900fd5fc38c80fb077ca2df0b7705418
Matt McNaney <mcnaneym@appstate.edu>
2020-06-02 14:31:20 -0400

    Tag changes remove need to have functions.

--- src/Tag/Script.php
+++ src/Tag/Script.php
@@ -12,17 +12,4 @@ namespace Canopy3\Tag;
 class Script extends \Canopy3\Tag
 {
 
-    public function __construct(array $attributes = null, string $tagName = null)
-    {
-        parent::__construct(['src' => $attributes['src'] ?? '', 'type' => $attributes['type'] ?? null]);
-        $this->isDefer($attributes['defer'] ?? false);
-        $this->singleton = false;
-    }
-
-    public function isDefer(bool $defer = true)
-    {
-        $this->setBoolAttribute('async', !$defer);
-        $this->setBoolAttribute('defer', $defer);
-    }
-
 }

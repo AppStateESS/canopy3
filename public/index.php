17e6a8a89f977fb557ba4133bb524c49e0da02b0
Matt McNaney <mcnaneym@appstate.edu>
2020-06-02 14:29:37 -0400

    Changed setup dashboard initialization logic

--- public/index.php
+++ public/index.php
@@ -6,18 +6,15 @@
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
  */
-$dbConfigFound = $urlConfigFound = true;
+require_once '../server.php';
 
-try {
-    require_once '../server.php';
-} catch (\Canopy3\Exception\FileNotFound $e) {
-    $urlConfigFound = false;
-}
 $dbConfigFound = is_file(C3_DIR . 'config/db.php');
 
-if (!$urlConfigFound || !$dbConfigFound) {
+if (!defined('C3_RESOURCE_URL') || !$dbConfigFound) {
     $setup = new \Dashboard\Setup\Setup;
     $setup->view();
 } else {
+    require_once C3_DIR . 'config/db.php';
+    require_once C3_DIR . 'config/resource.php';
     echo 'Admin dashboard is a go!';
 }

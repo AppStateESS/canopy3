b4d97691342e8f03389213871eb70cd23cd55fd7
Matt McNaney <mcnaneym@appstate.edu>
2020-06-26 14:59:53 -0400

    These are both works in progress.
    + Both classes are updated versions from Canopy. Experience with them
      will reshape their functionality.
    + Added in filter_input functions for security in SERVER dealings.

--- src/HTTP/Request.php
+++ src/HTTP/Request.php
@@ -6,7 +6,176 @@
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
  */
+
+namespace Canopy3\HTTP;
+
 class Request
 {
-//put your code here
+
+    private array $getVars;
+    private array $postVars;
+    private array $patchVars;
+    private array $putVars;
+    private array $deleteVars;
+    private string $method;
+    private string $requestUri;
+    static private Request $singletonObj;
+
+    private function __construct()
+    {
+        Server::getCurrentUri();
+        $this->loadRequestUri();
+        $this->method = strtoupper($_SERVER['REQUEST_METHOD']);
+        $this->loadData();
+    }
+
+    /**
+     * Checks to see the current require was an ajax request by reading the jquery
+     * header. JQUERY required for this.
+     * @return boolean
+     */
+    public static function isAjax()
+    {
+        return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest');
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isDelete()
+    {
+        return $this->method == 'DELETE';
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isGet()
+    {
+        return $this->method == 'GET';
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isPatch()
+    {
+        return $this->method == 'PATCH';
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isPost()
+    {
+        return $this->method == 'POST';
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isPut()
+    {
+        return $this->method == 'PUT';
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function getRequestUri()
+    {
+        return $this->requestUri;
+    }
+
+    public function loadData()
+    {
+        $data = file_get_contents('php://input');
+        $dataValues = array();
+        parse_str($data, $dataValues);
+        $content_type = isset($_SERVER['CONTENT_TYPE']) ? $_SERVER['CONTENT_TYPE'] : null;
+
+        switch ($this->method) {
+            case 'PATCH':
+                $this->setPatchVars($dataValues);
+                break;
+
+            case 'DELETE':
+                $this->setDeleteVars($dataValues);
+                break;
+
+            case 'PUT':
+                $this->setPutVars($dataValues);
+                break;
+
+            case 'POST':
+                if (strpos($content_type, 'multipart/form-data') !== false) {
+                    $this->setPostVars($_POST);
+                } else {
+                    $this->setPostVars($dataValues);
+                }
+                break;
+        }
+        $this->setGetVars($_GET);
+    }
+
+    /**
+     * @param array $vars
+     */
+    public function setDeleteVars($vars)
+    {
+        $this->deleteVars = $vars;
+    }
+
+    /**
+     * @param array $vars
+     */
+    public function setGetVars(array $vars)
+    {
+        $this->getVars = $vars;
+    }
+
+    /**
+     * @param array $vars
+     */
+    public function setPatchVars(array $vars)
+    {
+        $this->patchVars = $vars;
+    }
+
+    /**
+     * @param array $post
+     */
+    public function setPostVars(array $vars)
+    {
+        $this->postVars = $vars;
+    }
+
+    /**
+     * @param array $vars
+     */
+    public function setPutVars(array $vars)
+    {
+        $this->putVars = $vars;
+    }
+
+    public function shift()
+    {
+        var_dump($this);
+    }
+
+    public function singleton()
+    {
+        if (empty(self::$singletonObj)) {
+            self::$singletonObj = new self;
+        }
+        return self::$singletonObj;
+    }
+
+    private function loadRequestUri()
+    {
+        $this->requestUri = Server::getRequestUri();
+    }
+
 }
--- src/HTTP/Server.php
+++ src/HTTP/Server.php
@@ -1,6 +1,8 @@
 <?php
 
 /**
+ * Returns filtered information from the _SERVER super global and additional
+ * utilities.
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
@@ -11,19 +13,71 @@ namespace Canopy3\HTTP;
 class Server
 {
 
-    public static function getCurrentUrl($with_directory = true)
+    public static function getCurrentUri($with_directory = true)
     {
-        if (!isset($_SERVER['HTTP_HOST'])) {
-            throw new \Exception('$_SERVER[HTTP_HOST] superglobal does not exist');
-        }
+        $httpHost = self::getHost();
         $address[] = '//';
-        $address[] = $_SERVER['HTTP_HOST'];
+        $address[] = $httpHost;
         if ($with_directory) {
             $address[] = dirname($_SERVER['PHP_SELF']);
         }
 
         $url = preg_replace('@\\\@', '/', implode('', $address)) . '/';
         return $url;
+    }
+
+    public static function __callStatic(string $methodName, array $arguments)
+    {
+        $result = preg_split('/([A-Z][a-z]+)/', $methodName, -1,
+                PREG_SPLIT_DELIM_CAPTURE);
+
+        foreach ($result as $val) {
+            if ($val != 'get' && $val != '') {
+                $server[] = strtoupper($val);
+            }
+        }
+
+        $serverVal = implode('_', $server);
+        return self::getRequestVar($serverVal);
+    }
+
+    /**
+     * Returns request information following the site URI.
+     * @return string
+     */
+    public static function getRequestUriOnly()
+    {
+        $phpSelf = self::getPhpSelf();
+        $uri = self::getRequestUri();
+        $rootUri = substr($self, 0, strrpos($phpSelf, '/'));
+        $requestUri = preg_replace("@^$rootUri/@", '', $uri);
+        return empty($requestUri) ? false : $requestUri;
+    }
+
+    public static function getHost()
+    {
+        $httpHost = filter_input(INPUT_SERVER, 'HTTP_HOST',
+                FILTER_VALIDATE_DOMAIN);
+        if (empty($httpHost)) {
+            throw new \Exception('$_SERVER[HTTP_HOST] superglobal does not exist');
+        }
+        return $httpHost;
+    }
+
+    private static function getRequestVar(string $varName)
+    {
+        switch (strtoupper($varName)) {
+            case 'PHP_SELF':
+            case 'REQUEST_URI':
+            case 'SCRIPT_FILENAME':
+                return filter_input(INPUT_SERVER, $varName, FILTER_SANITIZE_URL);
+
+            case 'REQUEST_TIME':
+                return filter_input(INPUT_SERVER, $varName, FILTER_VALIDATE_INT);
+
+            default:
+                throw new \Exception("Unknown SERVER variable: $varname");
+        }
     }
 
 }

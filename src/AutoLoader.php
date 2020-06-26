87150f545f217c72829e2543fbb4fe7156d15c31
Matt McNaney <mcnaneym@appstate.edu>
2020-06-26 14:46:33 -0400

    Many comments added

--- src/AutoLoader.php
+++ src/AutoLoader.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- *
+ * Autoloader for Canopy and resource classes. The alternatives to Canopy
+ * are defined in the Libraries file.
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
@@ -11,7 +12,18 @@ spl_autoload_register(fn($namespaceString) => AutoLoader::run($namespaceString))
 class AutoLoader
 {
 
-    public static function run($namespaceString)
+    /**
+     * Receives a namespace string from the autoload register and determines if
+     * it is a Canopy class or a resource class.
+     *
+     * Depending on the type, the file path is returned and an attempted
+     * require_once is performed on it.
+     *
+     * @param string $namespaceString
+     * @return void
+     * @throws \Canopy3\Exception\AutoLoadFailure
+     */
+    public static function run(string $namespaceString)
     {
         $namespaceArray = explode('\\', $namespaceString);
         $libraryName = array_shift($namespaceArray);
@@ -26,19 +38,45 @@ class AutoLoader
         $call = self::getCall($libraryName, $params);
 
         require_once C3_DIR . "src/AutoLoader/$file";
-        $call($fileName, $directory);
+        $classFile = $call($fileName, $directory);
+        if (!is_file($classFile)) {
+            throw new \Canopy3\Exception\AutoLoadFailure($namespaceString,
+                    $classFile);
+        }
+        require_once $classFile;
     }
 
-    private static function getCall($library, $params)
+    /**
+     * Returns the name of the function specified by 'call' key in the
+     * $params array. The string will always be {$libraryName}Loader
+     *
+     * @param string $library
+     * @param array $params
+     * @return string
+     */
+    private static function getCall(string $libraryName, array $params)
     {
-        return $params['call'] ?? $library . 'Loader';
+        return $params['call'] ?? $libraryName . 'Loader';
     }
 
-    private static function getFile($libraryName, $params)
+    /**
+     * Returns the name of the file specified by the 'file' key in the
+     * $params array. The string will be {$libraryName}.php.
+     * 
+     * @param string $libraryName
+     * @param array $params
+     * @return string
+     */
+    private static function getFile(string $libraryName, array $params)
     {
         return $params['file'] ?? $libraryName . '.php';
     }
 
+    /**
+     * Returns the array of autoload libraries in the Libraries file. It assumes
+     * there is an autoloadLibraries variable in this file.
+     * @return array
+     */
     private static function getLibraries()
     {
         include C3_DIR . 'src/AutoLoader/Libraries.php';

ccf3ea53450521ceb3f26d6f84af25f94fb1843e
Matt McNaney <mcnaneym@appstate.edu>
2020-06-26 15:03:30 -0400

    This is an extendable Exception meant to be caught.
    Sometimes, you may want to throw an Exception but not have it disrupt
    the system. In this case, you can extend this class and check the class
    in the catch().

---
+++ src/Exception/CatchableException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * A CatchableException is an exception that is expected to be caught
+ * before it reported as a fatal error.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace Canopy3\Exception;
+
+class CatchableException extends \Exception
+{
+
+}

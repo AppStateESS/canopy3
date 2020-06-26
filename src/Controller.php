714c654a1149e7b9dbee3ce6e5c08f10341ff9ed
Matt McNaney <mcnaneym@appstate.edu>
2020-06-26 15:02:13 -0400

    Experimenting with a abstract Controller class.
    Currently, just a stub class. More to come.

---
+++ src/Controller.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace Canopy3;
+
+use Canopy3\Request;
+use Canopy3\Exception\UnknownRequestMethod;
+
+abstract class Controller
+{
+
+    protected \Canopy3\Request $request;
+
+    public function __construct()
+    {
+        $this->request = Request::singleton();
+    }
+
+    public function execute()
+    {
+        $method = strtoupper($this->request->getMethod());
+        switch ($method) {
+            case 'GET':
+                $response = $this->get();
+                break;
+            case 'HEAD':
+                $response = $this->head();
+                break;
+            case 'POST':
+                $response = $this->post();
+                break;
+            case 'PUT':
+                $response = $this->put();
+                break;
+            case 'DELETE':
+                $response = $this->delete();
+                break;
+            case 'OPTIONS':
+                $response = $this->options();
+                break;
+            case 'PATCH':
+                $response = $this->patch();
+                break;
+            default:
+                throw new UnknownRequestMethod($method);
+                break;
+        }
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function head()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+
+    public function put()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function options()
+    {
+
+    }
+
+    public function patch()
+    {
+
+    }
+
+}

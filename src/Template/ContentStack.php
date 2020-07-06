c7ea57ef8531e607b67c54ba144c17ebd5138f96
Matt McNaney <mcnaneym@appstate.edu>
2020-07-06 14:26:00 -0400

    Added wrap function
    + Added emptyResult function
    + Added comments

--- src/Template/ContentStack.php
+++ src/Template/ContentStack.php
@@ -58,19 +58,48 @@ class ContentStack
         echo $this->get($valueName);
     }
 
-    public function get(string $valueName)
+    private function emptyResult(string $valueName): string
     {
-        return $this->values[$valueName] ?? ($this->emptyWarning ? "<!-- Template variable [$valueName] is missing -->" : null);
+        return ($this->emptyWarning ? "<!-- Template variable [$valueName] is missing -->" : '');
     }
 
-    public function getValues()
+    public function get(string $valueName)
+    {
+        return $this->values[$valueName] ?? $this->emptyResult($valueName);
+    }
+
+    public function getValues(): array
     {
         return $this->values;
     }
 
-    public function include($fileName)
+    /**
+     * Returns a rendering of the $fileName using the current values. This
+     * allow embedding of smaller templates within a larger template.
+     *
+     * @param type $fileName
+     * @return string
+     */
+    public function include($fileName): string
     {
         return $this->template->render($fileName, $this->values);
+    }
+
+    /**
+     * If valueName has been set, it is returned surrounded by the text in the
+     * $left and $right parameters.
+     *
+     * @param type $valueName
+     * @param type $left
+     * @param type $right
+     * @return string
+     */
+    public function wrap($valueName, $left, $right): string
+    {
+        if (!isset($this->values[$valueName])) {
+            return $this->emptyResult($valueName);
+        }
+        return $left . $this->values[$valueName] . $right;
     }
 
 }

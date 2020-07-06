0be93f099c3e9388ce9b0a3111fceac14e3bf2e3
Matt McNaney <mcnaneym@appstate.edu>
2020-07-06 14:11:57 -0400

    Added new information about wrap function.

--- public/index.php
+++ public/index.php
@@ -1,20 +1,39 @@
 <?php
 
 /**
- *
+ * Public facing file for access. All admin access comes through this file.
+ * All site queries include this file.
+ * If the application has not been setup, it will be noticed and the router
+ * will be directed to run the install.
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
  */
 require_once '../server.php';
+require_once '../src/Router.php';
+
+use Canopy3\Router;
+use Canopy3\Theme;
+use Canopy3\Role;
+use Canopy3\ErrorPage;
+
+$theme = Theme::singleton();
+$router = Router::singleton();
 
 $dbConfigFound = is_file(C3_DIR . 'config/db.php');
 
-if (!defined('C3_RESOURCE_URL') || !$dbConfigFound) {
-    $setup = new \Dashboard\Setup\Setup;
-    $setup->view();
+if (!defined('C3_RESOURCES_URL') || !$dbConfigFound) {
+    $router->setResourceType('dashboard');
+    $router->setLibrary('Setup');
 } else {
     require_once C3_DIR . 'config/db.php';
-    require_once C3_DIR . 'config/resource.php';
-    echo 'Admin dashboard is a go!';
+    // determine router action from url
+}
+
+try {
+    $router->execute();
+} catch (\Canopy3\Exception\CodedException $e) {
+    echo ErrorPage::codedView($e);
+} catch (\Exception $e) {
+    echo ErrorPage::view($e);
 }

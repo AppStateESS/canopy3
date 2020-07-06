2c4acd2c9c66893fb9c552ce35f2e70b81e68147
Matt McNaney <mcnaneym@appstate.edu>
2020-07-06 14:26:17 -0400

    New config file for system

---
+++ config/system.default.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This configuration determines some system-wide settings. You should not make
+ * changes to this file. Instead make a copy named "system.php" and save it to
+ * the same directory.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+/**
+ * If true, then the program is set into development mode. Normally suppressed
+ * errors will print.
+ *
+ * Default: false
+ */
+define('C3_DEVELOPMENT_MODE', false);
+
+/**
+ * If true, any empty template value will show an empty template warning. You
+ * can use this to debug a template file.
+ */
+//define('C3_TEMPLATE_EMPTY_WARNING_DEFAULT', false);

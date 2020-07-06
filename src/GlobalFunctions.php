938333ff6b19fe59b41f4a611da0aba929c1771a
Matt McNaney <mcnaneym@appstate.edu>
2020-07-06 14:30:03 -0400

    New function only file.
    + Added requireConfigFile function.

---
+++ src/GlobalFunctions.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+use Canopy3\Exception\FileNotFound;
+
+/**
+ * Looks for and requires a file requested from the $filePath. If not found
+ * a default version is checked before throwing an exception.
+ * The directory is expected to be under the C3_DIR
+ *
+ * @param string $filePath
+ * @throws \FileNotFound
+ */
+function requireConfigFile(string $filePath)
+{
+    if (preg_match('/\.\./', $filePath)) {
+        throw new FileNotFound($filePath);
+    }
+
+    $fullPath = preg_replace('/\.php$/', '', C3_DIR . $filePath);
+    $configFile = $fullPath . '.php';
+    $defaultFile = $fullPath . '.default.php';
+
+    if (is_file($configFile)) {
+        require_once($configFile);
+    } elseif (is_file($defaultFile)) {
+        require_once($defaultFile);
+    } else {
+        throw new FileNotFound($configFile);
+    }
+}

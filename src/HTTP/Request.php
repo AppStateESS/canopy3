05e1d880ebde41db1e423b982cc1fb5923757230
Matt McNaney <mcnaneym@appstate.edu>
2020-07-15 14:25:27 -0400

    More work on Request. POST working properly now...
    by borrowing from old Request.
    + Each request type is now handled by the RequestType class.
    + Added a debug function for a quick output of what the request
      contains.

--- src/HTTP/Request.php
+++ src/HTTP/Request.php
@@ -9,23 +9,45 @@
 
 namespace Canopy3\HTTP;
 
+use Canopy3\HTTP\Request\RequestType;
+
 class Request
 {
 
-    private array $getVars;
-    private array $postVars;
-    private array $patchVars;
-    private array $putVars;
-    private array $deleteVars;
+    private RequestType $DELETE;
+    private RequestType $GET;
+    private RequestType $PATCH;
+    private RequestType $POST;
+    private RequestType $PUT;
     private string $method;
     private string $requestUri;
+
+    private const methodTypes = ['GET', 'DELETE', 'PATCH', 'POST', 'PUT'];
+
     static private Request $singletonObj;
 
     private function __construct()
     {
         $this->loadRequestUri();
         $this->method = strtoupper($_SERVER['REQUEST_METHOD']);
+        if (!$this->allowedMethod($this->method)) {
+            throw new InaccessibleProperty(__CLASS__, $requestVariable);
+        }
+        $this->DELETE = new RequestType('DELETE');
+        $this->GET = new RequestType('GET');
+        $this->PATCH = new RequestType('PATCH');
+        $this->POST = new RequestType('POST');
+        $this->PUT = new RequestType('PUT');
         $this->loadData();
+    }
+
+    public function __get($varName)
+    {
+        $requestVariable = strtoupper($varName);
+        if (!$this->allowedMethod($varName)) {
+            throw new InaccessibleProperty(__CLASS__, $requestVariable);
+        }
+        return $this->$requestVariable;
     }
 
     /**
@@ -88,87 +110,44 @@ class Request
         return $this->requestUri;
     }
 
-    public function loadData()
+    private function allowedMethod(string $method)
     {
-        $data = file_get_contents('php://input');
-        $dataValues = array();
-        parse_str($data, $dataValues);
-        $content_type = isset($_SERVER['CONTENT_TYPE']) ? $_SERVER['CONTENT_TYPE'] : null;
+        return in_array($method, self::methodTypes);
+    }
+
+    /**
+     * Pulls in data from the php input and applies the values to the current method object.
+     * GET always is filled.
+     */
+    private function loadData()
+    {
+        $contentType = $_SERVER['CONTENT_TYPE'] ?? null;
+        $formData = file_get_contents('php://input');
+        parse_str($formData, $dataValues);
 
         switch ($this->method) {
-            case 'PATCH':
-                $this->setPatchVars($dataValues);
-                break;
-
             case 'DELETE':
-                $this->setDeleteVars($dataValues);
-                break;
-
+            case 'PATCH':
             case 'PUT':
-                $this->setPutVars($dataValues);
+                $this->{$this->method}->setValues($dataValues);
                 break;
 
             case 'POST':
-                if (strpos($content_type, 'multipart/form-data') !== false) {
-                    $this->setPostVars($_POST);
+                var_dump($contentType);
+                if (strpos($contentType, 'multipart/form-data') !== false) {
+                    $this->{$this->method}->setValues($_POST);
                 } else {
-                    $this->setPostVars($dataValues);
+                    $this->{$this->method}->setValues($dataValues);
                 }
-                break;
         }
-        $this->setGetVars($_GET);
-    }
-
-    /**
-     * @param array $vars
-     */
-    public function setDeleteVars($vars)
-    {
-        $this->deleteVars = $vars;
-    }
-
-    /**
-     * @param array $vars
-     */
-    public function setGetVars(array $vars)
-    {
-        $this->getVars = $vars;
-    }
-
-    /**
-     * @param array $vars
-     */
-    public function setPatchVars(array $vars)
-    {
-        $this->patchVars = $vars;
-    }
-
-    /**
-     * @param array $post
-     */
-    public function setPostVars(array $vars)
-    {
-        $this->postVars = $vars;
-    }
-
-    /**
-     * @param array $vars
-     */
-    public function setPutVars(array $vars)
-    {
-        $this->putVars = $vars;
-    }
-
-    public function shift()
-    {
-        var_dump($this);
+        $this->GET->setValues($_GET);
     }
 
     /**
      *
      * @return Request
      */
-    public function singleton()
+    public static function singleton()
     {
         if (empty(self::$singletonObj)) {
             self::$singletonObj = new self;
@@ -179,6 +158,29 @@ class Request
     private function loadRequestUri()
     {
         $this->requestUri = Server::getRequestUri();
+    }
+
+    public function debug()
+    {
+        $values['GET'] = $this->templify($this->GET->getValues());
+        $values['DELETE'] = $this->templify($this->DELETE->getValues());
+        $values['GET'] = $this->templify($this->GET->getValues());
+        $values['PATCH'] = $this->templify($this->PATCH->getValues());
+        $values['POST'] = $this->templify($this->POST->getValues());
+        $values['PUT'] = $this->templify($this->PUT->getValues());
+        $template = new \Canopy3\Template(C3_DIR . 'src/HTTP/Request/Templates/');
+        return $template->render('Debug', $values);
+    }
+
+    private function templify($values)
+    {
+        if (empty($values)) {
+            return [];
+        }
+        foreach ($values as $key => $val) {
+            $row[] = ['key' => $key, 'value' => $val];
+        }
+        return $row;
     }
 
 }

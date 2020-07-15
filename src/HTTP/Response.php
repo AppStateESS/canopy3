baa9aa4fe8ecfe7459e064bf0e55e91ac668bd2f
Matt McNaney <mcnaneym@appstate.edu>
2020-07-15 14:27:16 -0400

    Response stub

--- src/HTTP/Response.php
+++ src/HTTP/Response.php
@@ -1,12 +1,20 @@
 <?php
 
 /**
- *
+ * Handles the response returned from processes within Canopy 3.
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
  */
+
+namespace Canopy3\HTTP;
+
 class Response
 {
-//put your code here
+
+    public function getHtml()
+    {
+
+    }
+
 }

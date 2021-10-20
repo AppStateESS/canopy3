d5db9227948738d04a7cb35df20aa417d334c6d6
Matt McNaney <mcnaneym@appstate.edu>
2021-10-20 10:45:29 -0400

    Commenting.

--- src/HTTP/Response/ResponseType.php
+++ src/HTTP/Response/ResponseType.php
@@ -7,9 +7,20 @@
 
 namespace Canopy3\HTTP\Response;
 
+/**
+ * A ResponseType is the final result of a process. Typically, this response
+ * is a communication string but it could a forward, an error, or a download prompt.
+ * The extended class requires an execute function that will be called by the router.
+ */
 abstract class ResponseType
 {
 
+    /**
+     * The HTTP response status code sent by the header.
+     * https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+     * Default on successful response is 200.
+     * @var int
+     */
     protected int $httpResponseCode = 200;
 
     public abstract function execute();

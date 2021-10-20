05139f4c739e61c43ddcdab02bfd0f41854d8d67
Matt McNaney <mcnaneym@appstate.edu>
2021-10-20 10:47:13 -0400

    License comments added.

--- src/Template/Value/Value.php
+++ src/Template/Value/Value.php
@@ -1,7 +1,10 @@
 <?php
 
 /**
+ * MIT License
+ * Copyright (c) 2021 Electronic Student Services @ Appalachian State University
  *
+ * See LICENSE file in root directory for copyright and distribution permissions.
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT

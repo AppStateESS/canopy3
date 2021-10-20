257857ff467e1287223efc45329aa5a2aa50599d
Matt McNaney <mcnaneym@appstate.edu>
2021-10-20 10:47:27 -0400

    New file just for directory defines.

---
+++ DirectoryDefines.php
@@ -0,0 +1,7 @@
+<?php
+
+define('C3_DIR', __DIR__ . '/');
+define('C3_THEMES_DIR', C3_DIR . 'resources/themes/');
+define('C3_DASHBOARDS_DIR', C3_DIR . 'resources/dashboards/');
+define('C3_PLUGINS_DIR', C3_DIR . 'resources/plugins/');
+define('C3_DATA_DIR', C3_DIR . 'data/');

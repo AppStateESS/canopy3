88cbc94428f52bf85e7c157155b354eaf50764ff
Matt McNaney <mcnaneym@appstate.edu>
2021-10-20 10:40:45 -0400

    Sorted functions.

--- resources/dashboards/Setup/src/View/SetupView.php
+++ resources/dashboards/Setup/src/View/SetupView.php
@@ -25,36 +25,36 @@ class SetupView
         Header::singleton()->setSiteTitle('Administration Setup');
     }
 
-    public function createResourcesConfig()
-    {
-        $header = Header::singleton();
-        $header->setPageTitle('Create Resources Config');
-        $header->addScript($this->javascriptUrl . 'updateResource.js',
-                ['defer' => true]);
-        $header->addScriptValue('resourcesUrl', C3_RESOURCES_URL);
-        $values['resourcesUrl'] = C3_RESOURCES_URL;
-        $values['c3Dir'] = C3_DIR;
-        $values['configWritable'] = is_writable(C3_DIR . 'config/');
-        return $this->wrapper('Create System File',
-                        $this->template->render('CreateSystemConfig', $values));
-    }
-
     public function createDatabaseConfig()
     {
         $header = Header::singleton()->addScript($this->javascriptUrl . 'databaseConfig.js',
-                ['defer' => true]);
+            ['defer' => true]);
         $configDirectory = C3_DIR . 'config/';
         $values = [];
         $values['configDirectory'] = $configDirectory;
         $values['configWritable'] = is_writable($configDirectory);
         return $this->wrapper('Create Database Configuration File',
-                        $this->template->render('DatabaseConfig', $values));
+                $this->template->render('DatabaseConfig', $values));
+    }
+
+    public function createResourcesConfig()
+    {
+        $header = Header::singleton();
+        $header->setPageTitle('Create Resources Config');
+        $header->addScript($this->javascriptUrl . 'updateResource.js',
+            ['defer' => true]);
+        $header->addScriptValue('resourcesUrl', C3_RESOURCES_URL);
+        $values['resourcesUrl'] = C3_RESOURCES_URL;
+        $values['c3Dir'] = C3_DIR;
+        $values['configWritable'] = is_writable(C3_DIR . 'config/');
+        return $this->wrapper('Create System File',
+                $this->template->render('CreateSystemConfig', $values));
     }
 
     public function resourceFileError()
     {
         return $this->wrapper('Resource config file failed',
-                        $this->template->render('ResourceFileError'));
+                $this->template->render('ResourceFileError'));
     }
 
     public function setupFile($setupFilePath)
@@ -65,16 +65,16 @@ class SetupView
 \$setupAllowed = true;
 EOF;
         $values['fileCode'] = $this->template->render('codeArea',
-                ['code' => $phpCode]);
+            ['code' => $phpCode]);
 
         $path = $setupFilePath;
         $consoleCode = <<<EOF
 echo '<&#63;php \$setupAllowed = true;' > $path
 EOF;
         $values['consoleCode'] = $this->template->render('codeArea',
-                ['code' => $consoleCode]);
+            ['code' => $consoleCode]);
         return $this->wrapper('Welcome to Canopy 3!',
-                        $this->template->render('setupFileWarning', $values));
+                $this->template->render('setupFileWarning', $values));
     }
 
     private function wrapper($title, $content)

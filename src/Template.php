f919ae88903b81c37004c9bfcd6fbcd2f104072a
Matt McNaney <mcnaneym@appstate.edu>
2021-10-20 10:46:44 -0400

    Commenting updated.
    Fixed exception use.
    htmlSuffix no longer allows any files other than html, txt, or phtml.
    Some function typing added.

--- src/Template.php
+++ src/Template.php
@@ -1,16 +1,23 @@
 <?php
 
 /**
+ * MIT License
+ * Copyright (c) 2021 Electronic Student Services @ Appalachian State University
  *
+ * See LICENSE file in root directory for copyright and distribution permissions.
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
+ */
+/**
+ * This class's purpose is to plug values into a PHP formatted HTML file and return the result.
+ * Full instructions on its use are in the markdown file /docs/Template.md
  */
 
 namespace Canopy3;
 
 use Canopy3\Template\ContentStack;
-use Canopy3\Exception\ExpectedType;
+use Canopy3\Exception\InvalidFileType;
 
 if (!defined('C3_TEMPLATE_EMPTY_WARNING_DEFAULT')) {
     define('C3_TEMPLATE_EMPTY_WARNING_DEFAULT', false);
@@ -19,7 +26,18 @@ if (!defined('C3_TEMPLATE_EMPTY_WARNING_DEFAULT')) {
 class Template
 {
 
+    /**
+     * Path to the directory containing the template files. Does NOT include
+     * in the file itself.
+     * @var string
+     */
     private string $path;
+
+    /**
+     * An array of functions that can be called in the template. New functions
+     * are pushed on the array using the registerFunction method.
+     * @var array
+     */
     private array $registeredFunctions = [];
 
     public function __construct(string $path = null)
@@ -40,17 +58,28 @@ class Template
         return C3_DIR . "resources/dashboards/$library/templates/";
     }
 
-    public function getPath()
+    public function getPath(): string
     {
         return $this->path;
     }
 
-    public static function htmlSuffix($fileName)
+    /**
+     * Checks $fileName parameter for
+     * @param string $fileName
+     * @return string
+     */
+    public static function htmlSuffix(string $fileName): string
     {
-        return preg_match('@\.(html|txt)$@', $fileName) ? $fileName : $fileName . '.html';
+        if (!preg_match('@\.(\w+)\w$@i', $fileName)) {
+            return $fileName . '.phtml';
+        } else if (preg_match('@\.(phtml|txt|html)$@i', $fileName)) {
+            return $fileName;
+        } else {
+            throw new InvalidFileType('html, txt, phtml', $fileName);
+        }
     }
 
-    public function isRegistered(string $functionName)
+    public function isRegistered(string $functionName): bool
     {
         return isset($this->registeredFunctions[$functionName]);
     }
@@ -66,13 +95,13 @@ class Template
     }
 
     public function registerFunction(string $functionName,
-            \Closure $functionCode)
+        \Closure $functionCode)
     {
         $this->registeredFunctions[$functionName] = $functionCode;
     }
 
     public function render(string $fileName, array $values = null,
-            bool $emptyWarning = C3_TEMPLATE_EMPTY_WARNING_DEFAULT): string
+        bool $emptyWarning = C3_TEMPLATE_EMPTY_WARNING_DEFAULT): string
     {
         $fileName = self::htmlSuffix($fileName);
         $filePath = $this->path . $fileName;
@@ -87,7 +116,15 @@ class Template
         return self::captureContent($t, $filePath);
     }
 
-    public function runRegistered(string $funcName, $value)
+    /**
+     * Calls a registered function by funcName using $value as the parameter.
+     *
+     * @param string $funcName
+     * @param mixed $value
+     * @return string
+     * @throws \Exception
+     */
+    public function runRegistered(string $funcName, $value): string
     {
         if (!is_callable($this->registeredFunctions[$funcName])) {
             throw new \Exception("Function [$funcName] was not registered");
@@ -95,7 +132,14 @@ class Template
         return $this->registeredFunctions[$funcName]($value);
     }
 
-    private static function captureContent(ContentStack $t, $filePath)
+    /**
+     * Receives the ContentStack object, applies it content values to the template file, outputs the content
+     * to the captured buffer, and returns the result.
+     * @param ContentStack $t
+     * @param type $filePath
+     * @return type
+     */
+    private static function captureContent(ContentStack $t, $filePath): string
     {
         ob_start();
         include $filePath;

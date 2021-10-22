854d24fffc2daffe5c9414374e7047d171007705
Matt McNaney <mcnaneym@appstate.edu>
2021-10-22 13:09:10 -0400

    Interface for client based exceptions added.

---
+++ src/Exception/Client/ClientException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * MIT License
+ * Copyright (c) 2021 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * This interface can be implemented to mark an exception. ClientExceptions are error resulting
+ * specifically from client access.
+ * Exceptions arising from unexpected server behavior should not implement this interface.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace Canopy3\Exception\Client;
+
+interface ClientException
+{
+
+}

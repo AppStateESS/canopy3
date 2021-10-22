dcfbb7b10d637b6caa087c8a98d2083a2d752af4
Matt McNaney <mcnaneym@appstate.edu>
2021-10-22 13:01:10 -0400

    Added C3_RESOURCES_DIR

--- DirectoryDefines.php
+++ DirectoryDefines.php
@@ -1,7 +1,8 @@
 <?php
 
 define('C3_DIR', __DIR__ . '/');
-define('C3_THEMES_DIR', C3_DIR . 'resources/themes/');
-define('C3_DASHBOARDS_DIR', C3_DIR . 'resources/dashboards/');
-define('C3_PLUGINS_DIR', C3_DIR . 'resources/plugins/');
+define('C3_RESOURCES_DIR', C3_DIR . 'resources/');
+define('C3_DASHBOARDS_DIR', C3_RESOURCES_DIR . 'dashboards/');
+define('C3_PLUGINS_DIR', C3_RESOURCES_DIR . 'plugins/');
+define('C3_THEMES_DIR', C3_RESOURCES_DIR . 'themes/');
 define('C3_DATA_DIR', C3_DIR . 'data/');

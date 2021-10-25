0b843ceeaa8d14f986f310500b3bc52dc7f00c46
Matt McNaney <mcnaneym@appstate.edu>
2021-10-25 14:09:27 -0400

    Added homeUrl.

--- src/Template/ContentStack.php
+++ src/Template/ContentStack.php
@@ -11,7 +11,7 @@
  */
 /**
  * The ContentStack objects is what is accessed in the template file itself. It is expressed in the $t variable.
- * 
+ *
  * Example:
  *
  * <p><?=$t->valueName?></p>
@@ -19,22 +19,27 @@
 
 namespace Canopy3\Template;
 
-use Canopy3\Template\Value\Value;
+use Canopy3\HTTP\Server;
 use Canopy3\Template;
+use Canopy3\Template\Value\Value;
 
 class ContentStack
 {
 
+    public $template;
     protected $emptyWarning = false;
     protected $filePath;
-    public $template;
     protected $values;
+    private static string $homeUrl;
 
     public function __construct($template, array $values, $emptyWarning = false)
     {
+        if (empty($homeUrl)) {
+            $this->homeUrl = Server::getCurrentUri();
+        }
         $this->template = $template;
         $this->emptyWarning = $emptyWarning;
-
+        $this->values['homeUrl'] = $this->homeUrl;
         foreach ($values as $k => $v) {
             if (is_a($v, 'Canopy3\Template\Value\Value')) {
                 $this->values[$k] = $v;
@@ -92,7 +97,7 @@ class ContentStack
      */
     public function include(string $fileName): string
     {
-        return $this->template->render($fileName, $this->values);
+        return $this->template->render($fileName, $values);
     }
 
     /**

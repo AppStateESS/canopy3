edd0d57a7f1b437753f4cbc7087606e275ce2017
Matt McNaney <mcnaneym@appstate.edu>
2021-11-01 13:45:09 -0400

    Added config directory.

--- DirectoryDefines.php
+++ DirectoryDefines.php
@@ -1,6 +1,7 @@
 <?php
 
 define('C3_DIR', __DIR__ . '/');
+define('C3_CONFIG_DIR', C3_DIR . 'config/');
 define('C3_RESOURCES_DIR', C3_DIR . 'resources/');
 define('C3_DASHBOARDS_DIR', C3_RESOURCES_DIR . 'dashboards/');
 define('C3_PLUGINS_DIR', C3_RESOURCES_DIR . 'plugins/');

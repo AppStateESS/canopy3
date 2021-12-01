f5d8ad2092324ce537db5f99a6f08f048ae0e760
Matt McNaney <mcnaneym@appstate.edu>
2021-12-01 09:08:55 -0500

    Added phpdoc comments.
    Added property types using colons.
    Types appear in phpdoc and intialization.

--- bin/src/createClass/SubTemplates.php
+++ bin/src/createClass/SubTemplates.php
@@ -9,27 +9,70 @@
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
  */
-function propertyTpl($value)
+function propertyTpl($property)
 {
-    return "    private \$$value;\n";
+    extract(typeAndValue($property));
+    return <<<EOF
+    /**
+     * @var $type
+     */
+    private $type \$$value;
+
+EOF;
 }
 
-function getTpl(string $value): string
+function getComment($type = null)
 {
+    return <<<EOF
+    /**
+     * @returns $type
+     */
+EOF;
+}
+
+function setComment($param, $type = null)
+{
+    return <<<EOF
+    /**
+     * @param $type \$$param
+     */
+EOF;
+}
+
+function typeAndValue($property)
+{
+    if (strpos($property, ':') !== -1) {
+        list($value, $type) = explode(':', $property);
+    } else {
+        $value = $property;
+        $type = null;
+    }
+    return ['value' => $value, 'type' => $type];
+}
+
+function getTpl(string $property): string
+{
+    extract(typeAndValue($property));
     $funcName = 'get' . ucwords($value);
-    $funcStack[] = "    public function $funcName()";
-    $funcStack[] = '    {';
-    $funcStack[] = '        return $this->' . $value . ';';
-    $funcStack[] = '    }';
-    $funcStack[] = '';
+    $funcStack[0] = getComment($type);
+    $funcStack[1] = "    public function $funcName()";
+    if ($type) {
+        $funcStack[1] .= ' : ' . $type;
+    }
+    $funcStack[2] = '    {';
+    $funcStack[3] = '        return $this->' . $value . ';';
+    $funcStack[4] = '    }';
+    $funcStack[5] = '';
 
     return implode("\n", $funcStack);
 }
 
-function setTpl(string $value): string
+function setTpl(string $property): string
 {
+    extract(typeAndValue($property));
     $funcName = 'set' . ucwords($value);
-    $funcStack[] = '    public function ' . $funcName . '($' . $value . ')';
+    $funcStack[] = setComment($value, $type);
+    $funcStack[] = '    public function ' . $funcName . '(' . $type . ' $' . $value . ')';
     $funcStack[] = '    {';
     $funcStack[] = '        $this->' . $value . ' = $' . $value . ';';
     $funcStack[] = '    }';

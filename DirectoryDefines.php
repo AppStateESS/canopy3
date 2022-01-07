775b3fc9c1b2e28d879c5564d0cec070617dcee0
Matt McNaney <mcnaneym@appstate.edu>
2022-01-07 09:02:46 -0500

    RENAMED C3_RESOURCES_DIR to C3_SYSTEMS_DIR

--- DirectoryDefines.php
+++ DirectoryDefines.php
@@ -2,8 +2,8 @@
 
 define('C3_DIR', __DIR__ . '/');
 define('C3_CONFIG_DIR', C3_DIR . 'config/');
-define('C3_RESOURCES_DIR', C3_DIR . 'resources/');
-define('C3_DASHBOARDS_DIR', C3_RESOURCES_DIR . 'dashboards/');
-define('C3_PLUGINS_DIR', C3_RESOURCES_DIR . 'plugins/');
-define('C3_THEMES_DIR', C3_RESOURCES_DIR . 'themes/');
+define('C3_SYSTEMS_DIR', C3_DIR . 'systems/');
+define('C3_DASHBOARDS_DIR', C3_SYSTEMS_DIR . 'dashboards/');
+define('C3_PLUGINS_DIR', C3_SYSTEMS_DIR . 'plugins/');
+define('C3_THEMES_DIR', C3_SYSTEMS_DIR . 'themes/');
 define('C3_DATA_DIR', C3_DIR . 'data/');

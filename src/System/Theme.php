c00cc6e94e6ef632d6153414d1bd6f6aed4cc7de
Matt McNaney <mcnaneym@appstate.edu>
2022-01-13 16:00:12 -0500

    Added Theme system.

---
+++ src/System/Theme.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace Canopy3\System;
+
+class Theme extends AbstractSystem
+{
+
+    protected string $type = 'canopy3-theme';
+
+}

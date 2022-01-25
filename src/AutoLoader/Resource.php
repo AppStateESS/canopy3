5f4d64225c26c4183e3edc5e836cae02cafeadac
Matt McNaney <mcnaneym@appstate.edu>
2022-01-25 08:23:38 -0500

    Rewrote autoloaders.

--- src/AutoLoader/Resource.php
+++ src/AutoLoader/Resource.php
@@ -4,29 +4,22 @@
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
  */
-require_once C3_DIR . 'src/Dashboard.php';
-require_once C3_DIR . 'src/Plugin.php';
+require_once C3_DIR . 'src/SystemFactory.php';
 
 function DashboardLoader(string $filename, string $classDirectory)
 {
     $dashboard = stristr($classDirectory, '/', true);
-    if ($dashboard) {
-        $dashboardDirectory = \Canopy3\Dashboard::singleton()->getDirectory($dashboard);
-        $className = stristr($classDirectory, '/');
-        return C3_DASHBOARDS_DIR . "$dashboardDirectory/src{$className}/{$filename}.php";
-    } else {
-        return C3_DASHBOARDS_DIR . "$classDirectory/src/{$filename}.php";
-    }
+    $dashboardDirectory = \Canopy3\SystemFactory::getDashboardDirectoryByNamespace($dashboard) ?? $dashboard;
+    $className = stristr($classDirectory, '/');
+
+    return C3_DASHBOARDS_DIR . "$dashboardDirectory/src{$className}/{$filename}.php";
 }
 
 function PluginLoader(string $filename, string $classDirectory)
 {
     $plugin = stristr($classDirectory, '/', true);
-    if ($plugin) {
-        $pluginDirectory = \Canopy3\Plugin::singleton()->getDirectory($plugin);
-        $className = stristr($classDirectory, '/');
-        return C3_DASHBOARDS_DIR . "$pluginDirectory/src{$className}/{$filename}.php";
-    } else {
-        return C3_DASHBOARDS_DIR . "$classDirectory/src/{$filename}.php";
-    }
+    $pluginDirectory = \Canopy3\SystemFactory::getDashboardDirectoryByNamespace($plugin) ?? $plugin;
+    $className = stristr($classDirectory, '/');
+
+    return C3_DASHBOARDS_DIR . "$pluginDirectory/src{$className}/{$filename}.php";
 }
